4eae89652d9750837c3d87e9f43a5024b052d9cb
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-03-06 20:15:01 -0600

    marck
    traslado de disco duro y Empleados

--- src/controllers/empresa.controller.js
+++ src/controllers/empresa.controller.js
@@ -50,33 +50,36 @@ function EditarEmpresas(req, res) {
         return res.status(200).send({ empresa: empresaEditada});
     })
 }
-/*
-function EliminarProductos(req, res) {
-    var idProd = req.params.idProducto;
 
-    Productos.findByIdAndDelete(idProd, (err, productoEliminado)=>{
-        if(err) return res.status(500).send({ mensaje: 'Error en la peticion' });
-        if(!productoEliminado) return res.status(500)
-            .send({ mensaje: 'Error al eliminar el producto' })
+function EliminarEmpresas(req, res) {
+    var idEm = req.params.idEmpresa;
 
-        return res.status(200).send({ producto: productoEliminado });
+    Empresas.findByIdAndDelete(idEm, (err,  empresaEditada)=>{
+        if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+        if(!empresaEditada) return res.status(404)
+            .send({ mensaje: 'Error al Editar la Empresa' });
+
+        return res.status(200).send({ empresa: empresaEditada});
     })
 }
 
-function agregarProveedor(req, res) {
+
+function agregarEmpleados(req, res) {
     const parametros = req.body;
-    const modeloProveedor = new Proveedor();
+    const modeloEmpleados = new Empleados();
 
-    if(parametros.nombreProveedor && parametros.direccion){
+    if(parametros.nombre && parametros.apellido && parametros.edad && parametros.trabajo){
 
-        modeloProveedor.nombreProveedor = parametros.nombreProveedor;
-        modeloProveedor.direccion = parametros.direccion;
+        modeloEmpleados.nombre = parametros.nombre;
+        modeloEmpleados.apellido = parametros.apellido;
+        modeloEmpleados.edad = parametros.edad;
+        modeloEmpleados.trabajo = parametros.trabajo;
 
-        modeloProveedor.save((err, proveedorGuardado) => {
+        modeloEmpleados.save((err, empleadoGuardado) => {
             if(err) return res.status(500).send({ mensaje: 'Error en la peticion'});
-            if(!proveedorGuardado) return res.status(500).send({ mensaje: 'Error al guardar el proveedor'});
+            if(!empleadoGuardado) return res.status(500).send({ mensaje: 'Error al guardar el empleado'});
 
-            return res.status(200).send({ proveedor: proveedorGuardado})
+            return res.status(200).send({ empleado: empleadoGuardado})
         })
 
     } else {
@@ -86,34 +89,65 @@ function agregarProveedor(req, res) {
 
 }
 
-function agregarProvedorProducto(req, res) {
-    var productoId = req.params.idProducto;
-    var proveedorId = req.params.idProveedor;
 
-    Productos.findByIdAndUpdate(productoId, { $push: {  provedores : { idProveedor: proveedorId } } }, {new : true}, 
-        (err, proveedorAgregado) => {
+function agregarEmpleadoEmpresa(req, res) {
+    var idEmpre = req.params.idEmpresa;
+    var idEmple = req.params.idEmpleado;
+
+    Empresas.findByIdAndUpdate(idEmpre, { $push: { empleados : { idEmpleado: idEmple } } }, {new : true}, 
+        (err, empleadoAgregado) => {
             if(err) return res.status(500).send({ mensaje: 'Error en  la peticion'});
-            if(!proveedorAgregado) return res.status(500).send({ mensaje: 'Error al agregar el proveedor al producto'});
+            if(!empleadoAgregado) return res.status(500).send({ mensaje: 'Error al agregar el empleado a la empresa'});
 
             return res.status(200).send({ product: proveedorAgregado });
         })
 }
 
-function buscarProductoXProveedor(req, res) {
-    const proveedorId = req.params.idProveedor;
 
-    Productos.find({ provedores : { $elemMatch: { _id: proveedorId } } }, (err, productosEncontrados)=>{
+function buscarEmpresaXEmpleados(req, res) {
+    var idEmple = req.params.idEmpleado;
+
+    Empresas.find({ empleados : { } }, (err, empresasEncontradas)=>{
         if(err) return res.status(500).send({ mensaje: 'Error en  la peticion'});
-        if(!productosEncontrados) return res.status(500).send({ mensaje: 'Error al obtener los productos por proveedor'});
+        if(!empresasEncontradas) return res.status(500).send({ mensaje: 'Error al obtener los empleados por empresas'});
 
         return res.status(200).send({ productos: productosEncontrados })
     })
     
-}*/
+}
+
+function buscarEmpresaXEmpleados(req, res) {
+    var idEmple = req.params.idEmpleado;
+
+    Empresas.find({ empleados : { $elemMatch: { _id: idEmple,  } } }, (err, empresasEncontradas)=>{
+        if(err) return res.status(500).send({ mensaje: 'Error en  la peticion'});
+        if(!empresasEncontradas) return res.status(500).send({ mensaje: 'Error al obtener los empleados por empresas'});
+
+        return res.status(200).send({ productos: productosEncontrados })
+    })
+    
+}
+
+function buscarDatosEmpleado(req, res) {
+    var parametros = req.body;
+
+    Usuario.find({ nombre: parametros.nombre, apellido: parametros.apellido }, 
+        { nombre: 1 }, (err, usuariosEncontrados) => {
+            if(err) return res.status(500).send({ mensaje: 'Error en  la peticion'});
+            if(!usuariosEncontrados) return res.status(500)
+                .send({ mensaje: 'Error al obtener los usuarios'})
+
+            return res.status(200).send({ usuarios: usuariosEncontrados })
+    })
+}
 
 module.exports = {
 
     AgregarEmpresas,
-    EditarEmpresas
+    EditarEmpresas,
+    EliminarEmpresas,
+    agregarEmpleados,
+    agregarEmpleadoEmpresa,
+    buscarEmpresaXEmpleados
 
 }
